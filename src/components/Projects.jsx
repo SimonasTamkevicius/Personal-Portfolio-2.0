57c6cba32887ee7d1a9ebd5d4df0cd2f53e9cdba
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-11-27 19:07:26 -0500

    fixed the tech stack icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,51 +18,77 @@ const Projects = () => {
         </div>
         <div className='flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 px-10 lg:px-20 py-20 space-y-5 md:space-y-0 md:gap-5 overflow-hidden'>
           {/* bead boutique project card */}
-          <div className='flex flex-col border-2 border-black rounded-lg shadow-xl project-card-1'>
-            <div className='flex flex-col justify-between mx-5 lg:mx-5 space-y-5 my-5'>
-                <h1 className='text-2xl font-semibold project-1-title'>the bead boutique</h1>
-                <p className='text-sm'>
-                    This is a comprehensive e-commerce platform equipped with administrative features, allowing administrators to effortlessly modify and add products, access order information, and manage the customer base.
-                </p>
-              <div className='flex flex-col'>
+          <div className='flex flex-col border-2 border-black rounded-lg shadow-xl project-card-1 h-full'>
+            <div className='flex flex-col justify-between mx-5 lg:mx-5 space-y-5 my-5 h-full'>
+              <h1 className='text-2xl font-semibold project-1-title'>the bead boutique</h1>
+              <p className='text-sm'>
+                This is a comprehensive e-commerce platform equipped with administrative features, allowing administrators to effortlessly modify and add products, access order information, and manage the customer base.
+              </p>
+              <div className='flex flex-col justify-between h-full'>
                 <h1 className='font-bold'>built using:</h1>
-                <div className='flex flex-row justify-start items-center space-x-3 mt-2'>
-                    <img src='/reactIcon.png' className='h-6' />
-                    <img src='/nodeJSIcon.png' className='h-6' />
-                    <img src='/mongoDBIcon.png' className='h-6' />
-                    <img src='/awsIcon.png' className='h-6' />
+                <div className='flex flex-wrap justify-start items-center gap-5 mt-2'>
+                  <div className='icon-container'>
+                    <img src='/reactIcon.png' className='h-6' alt='React' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/nodeJSIcon.png' className='h-6' alt='Node.js' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/expressJSIcon.png' className='h-6' alt='Express.js' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/mongoDBIcon.png' className='h-6' alt='MongoDB' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/awsIcon.png' className='h-6' alt='AWS' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/stripeIcon.png' className='h-6' alt='Stripe' />
+                  </div>
                 </div>
-              </div>
-              <a href='https://ecommerce-bead-store.onrender.com' target='_blank' className='flex justify-end items-end'>
-                <div className='flex flex-row justify-center items-center space-x-2 border border-black rounded-xl p-2 visit-button'>
+                <a href='https://ecommerce-bead-store.onrender.com' target='_blank' className='flex justify-end items-end mt-auto pt-5'>
+                  <div className='flex flex-row justify-center items-center space-x-2 border border-black rounded-xl p-2 visit-button'>
                     <p>visit</p>
                     <LuExternalLink className='external-link text-xl font-semibold' />
-                </div>
-              </a>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
           {/* Comilla Inc. project card */}
-          <div className='flex flex-col border-2 border-black rounded-lg shadow-xl project-card-2'>
-            <div className='flex flex-col justify-between mx-5 lg:mx-5 space-y-5 my-5'>
-                <h1 className='text-2xl font-semibold project-2-title'>comilla inc.</h1>
-                <p className='text-sm'>
-                    This website was crafted for a client engaged in electrical contracting services. The platform provides the administrator with the capability to seamlessly add new projects and upcoming events.
-                </p>
-              <div className='flex flex-col'>
+          <div className='flex flex-col justify-between border-2 border-black rounded-lg shadow-xl project-card-2'>
+            <div className='flex flex-col justify-between mx-5 lg:mx-5 space-y-5 my-5 h-full'>
+              <h1 className='text-2xl font-semibold project-2-title'>comilla inc.</h1>
+              <p className='text-sm'>
+                This website was crafted for a client engaged in electrical contracting services. The platform provides the administrator with the capability to seamlessly add new projects and upcoming events.
+              </p>
+              <div className='flex flex-col justify-between h-full mt-auto'>
                 <h1 className='font-bold'>built using:</h1>
-                <div className='flex flex-wrap justify-start items-center space-x-3 mt-2'>
-                    <img src='/reactIcon.png' className='h-6' />
-                    <img src='/nodeJSIcon.png' className='h-6' />
-                    <img src='/mongoDBIcon.png' className='h-6' />
-                    <img src='/awsIcon.png' className='h-6' />
+                <div className='flex flex-wrap justify-start items-center gap-5 mt-2'>
+                  <div className='icon-container'>
+                    <img src='/reactIcon.png' className='h-6' alt='React' />
+                  </div>
+                  {/* Add other icon containers here */}
+                  <div className='icon-container'>
+                    <img src='/nodeJSIcon.png' className='h-6' alt='Node.js' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/expressJSIcon.png' className='h-6' alt='Express.js' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/mongoDBIcon.png' className='h-6' alt='MongoDB' />
+                  </div>
+                  <div className='icon-container'>
+                    <img src='/awsIcon.png' className='h-6' alt='AWS' />
+                  </div>
                 </div>
-              </div>
-              <a href='https://comillainc.com' target='_blank' className='flex justify-end items-end'>
-                <div className='flex flex-row justify-center items-center space-x-2 border border-black rounded-xl p-2 visit-button'>
+                <a href='https://comillainc.com' target='_blank' className='flex justify-end items-end mt-auto'>
+                  <div className='flex flex-row justify-center items-center space-x-2 border border-black rounded-xl p-2 visit-button'>
                     <p>visit</p>
                     <LuExternalLink className='external-link text-xl font-semibold' />
-                </div>
-              </a>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
           {/*Coming soon project card*/}
