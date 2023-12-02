055e8d69a33cc335bb31e6c79be521ffed67231d
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-12-02 12:01:30 -0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/contact page/Contact';
+import { VercelAnalytics } from '@vercel/analytics';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </HashRouter>
+    <>
+      <VercelAnalytics />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </HashRouter>
+    </>
   )
 }
 
